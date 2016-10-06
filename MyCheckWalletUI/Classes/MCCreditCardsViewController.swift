ef39758e40bca20ef139ae9e2ad704289ddc6a2f
Mihail Kalichkov <mihailk@mycheckapp.com>
2016-10-06 16:42:01 +0300

    WAL-152 - made cc field bigger

--- MyCheckWalletUI/Classes/MCCreditCardsViewController.swift
+++ MyCheckWalletUI/Classes/MCCreditCardsViewController.swift
@@ -43,12 +43,6 @@ internal class MCCreditCardsViewController: MCViewController , UIScrollViewDeleg
         let addCreditCardView = AddCreditCardView(frame: CGRectMake(0, 20, 168, 104) )
         self.scrollView.addSubview(addCreditCardView)
         
-        let testMEthod1 = PaymentMethod(JSON: ["id":4, "token" : "5", "exp_month" : 2, "exp_year4" : 2002, "last_4_digits" : "2131", "is_default" : 1, "is_single_use" : 0, "issuer_short" : "visa", "issuer_full" : "visa"])
-        let testMEthod2 = PaymentMethod(JSON: ["id":5, "token" : "6", "exp_month" : 3, "exp_year4" : 2003, "last_4_digits" : "2133", "is_default" : 0, "is_single_use" : 0, "issuer_short" : "mastercard", "issuer_full" : "mastercard"])
-        let testMEthod3 = PaymentMethod(JSON: ["id":6, "token" : "7", "exp_month" : 4, "exp_year4" : 2004, "last_4_digits" : "2134", "is_default" : 0, "is_single_use" : 0, "issuer_short" : "amex", "issuer_full" : "amex"])
-        let testMEthod4 = PaymentMethod(JSON: ["id":7, "token" : "8", "exp_month" : 5, "exp_year4" : 2005, "last_4_digits" : "2135", "is_default" : 0, "is_single_use" : 0, "issuer_short" : "discover", "issuer_full" : "discover"])
-        self.paymentMethods = [testMEthod1!, testMEthod2!, testMEthod3!, testMEthod4!]
-        creditCardCount = self.paymentMethods.count
         for i in (0..<creditCardCount) {
             let method = self.paymentMethods[i]
             let cc = CreditCardView(frame: CGRectMake(193*CGFloat(i+1), 20, 164, 102), method: method)
