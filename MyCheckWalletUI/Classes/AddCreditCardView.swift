f013fda132895af45ce9f06ce7b807fa58acfc42
elad schiller <eladsc@mycheck.co.il>
2017-11-06 11:35:47 +0200

    BP-333 add credit card dynamic text

--- MyCheckWalletUI/Classes/AddCreditCardView.swift
+++ MyCheckWalletUI/Classes/AddCreditCardView.swift
@@ -10,32 +10,61 @@ import UIKit
 import MyCheckCore
 
 class AddCreditCardView: UIView , UIGestureRecognizerDelegate{
+  
+  var paymentMethod : PaymentMethodInterface?
+  
+
+  
+  override init(frame: CGRect) {
+    super.init(frame: frame)
     
-    var paymentMethod : PaymentMethodInterface?
+    setImageView(frame)
+    let recognizer = UITapGestureRecognizer(target: self, action: #selector(AddCreditCardView.buttonPressed(_:)))
+    recognizer.delegate = self;
+    self.addGestureRecognizer(recognizer)
+    addAddCreditCardLabel()
+  }
+  
+  required init?(coder aDecoder: NSCoder) {
+    super.init(coder: aDecoder)
+  }
+  
+  func buttonPressed(_ recognizer : UITapGestureRecognizer) {
+    NotificationCenter.default.post(name: Notification.Name(rawValue: "AddCreditCardPressed"), object: nil)
     
-    override init(frame: CGRect) {
-        super.init(frame: frame)
-        
-        let bundle =  MCViewController.getBundle( Bundle(for: MCAddCreditCardViewController.classForCoder()))
-        let image = UIImageView(image: UIImage(named: "addcreditcardbackground", in: bundle, compatibleWith: nil)!)
-        var imgFrame = frame
-        imgFrame.origin.x = 0
-        imgFrame.origin.y = 0
-        
-        image.frame = imgFrame
-        self.addSubview(image)
-        image.contentMode = .scaleAspectFit
-        let recognizer = UITapGestureRecognizer(target: self, action: #selector(AddCreditCardView.buttonPressed(_:)))
-        recognizer.delegate = self;
-        self.addGestureRecognizer(recognizer)
-    }
+  }
+  
+ 
+}
+
+fileprivate extension AddCreditCardView{
+  
+   func setImageView(_ frame: CGRect) {
+    let bundle =  MCViewController.getBundle( Bundle(for: MCAddCreditCardViewController.classForCoder()))
+    let image = UIImage(named: "addcreditcardbackground", in: bundle, compatibleWith: nil)!
+    let tintableImage = image.withRenderingMode(.alwaysTemplate)
+    let imageView = UIImageView(image: tintableImage)
     
-    required init?(coder aDecoder: NSCoder) {
-        super.init(coder: aDecoder)
-    }
+    var imgFrame = frame
+    imgFrame.origin.x = 0
+    imgFrame.origin.y = 0
     
-    func buttonPressed(_ recognizer : UITapGestureRecognizer) {
-        NotificationCenter.default.post(name: Notification.Name(rawValue: "AddCreditCardPressed"), object: nil)
-        
-    }
+    imageView.frame = imgFrame
+    self.addSubview(imageView)
+    imageView.contentMode = .scaleAspectFit
+    imageView.tintColor = LocalData.manager.getAddCreditCardTintColor()
+
+  }
+  
+  func addAddCreditCardLabel() {
+    let label = UILabel(frame: CGRect(x: 0, y: 0, width: self.frame.width, height: 22))
+    let center = CGPoint(x: self.frame.size.width / 2, y: self.frame.size.height - label.frame.size.height )
+    label.center  = center
+    label.font = UIFont.ragularFont(withSize: label.font.pointSize)
+    label.textAlignment = .center
+    
+    label.text = LocalData.manager.getAddCreditCardText()
+    label.textColor = LocalData.manager.getAddCreditCardTextColor()
+    self .addSubview(label)
+  }
 }
---
+++ MyCheckWalletUI/Classes/extensionTools/LocalData+MCCreditCardsViewController.swift
@@ -0,0 +1,32 @@
+//
+//  LocalData+MCCreditCardsViewController.swift
+//  MyCheckWalletUI
+//
+//  Created by elad schiller on 06/11/2017.
+//
+
+import Foundation
+
+
+internal extension LocalData{
+  
+  internal func getAddCreditCardText(fallback: String? = nil) -> String{
+    
+    return getString("managePaymentMethodsaddCard" , fallback: fallback)
+  }
+
+  
+  internal func getAddCreditCardTintColor() -> UIColor{
+    return getColor("managePaymentMethodscolorsaddCardTint" , fallback: UIColor.gray)
+  }
+  
+  internal func getAddCreditCardTextColor() -> UIColor{
+    return getColor("managePaymentMethodscolorsaddCardText" , fallback: UIColor.gray)
+  }
+  
+  internal func getAddCreditCardImageURL() -> URL?{
+    
+    return URL(string:getString("managePaymentMethodsimagesaddCard"))
+  }
+}
+
